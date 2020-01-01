6bd4d56a05352d15de455f63989b3352954ac0b3
Jason Chen <superj63@hotmail.com>
2020-01-01 18:05:41 +0800

    scraper done per meet

--- scraper.py
+++ scraper.py
@@ -6,13 +6,27 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
+import pandas as pd
 
 #starting webdriver
-BASE_URL = "https://racing.hkjc.com/racing/information/English/racing/LocalResults.aspx"
-
+BASE_URL = "https://racing.hkjc.com/racing/information/English/racing/LocalResults.aspx?RaceDate="
+dates = ["29/12/2019", "26/12/2019", "21/12/2019", "18/12/2019", "15/12/2019", "11/12/2019", "08/12/2019",
+ "04/12/2019", "01/12/2019", "27/11/2019", "24/11/2019", "23/11/2019", "20/11/2019", "17/11/2019", "13/11/2019", "10/11/2019", "09/11/2019",
+  "06/11/2019", "05/11/2019", "03/11/2019", "30/10/2019", "27/10/2019", "23/10/2019", "20/10/2019", "19/10/2019", "16/10/2019", "13/10/2019",
+   "12/10/2019", "09/10/2019", "06/10/2019", "01/10/2019", "29/09/2019", "25/09/2019", "21/09/2019", "18/09/2019", "15/09/2019", "11/09/2019",
+    "08/09/2019", "01/09/2019", "14/07/2019", "10/07/2019", "07/07/2019", "03/07/2019", "01/07/2019", "26/06/2019", "23/06/2019", "22/06/2019", "21/06/2019",
+     "20/06/2019", "19/06/2019", "18/06/2019", "16/06/2019", "12/06/2019", "08/06/2019", "05/06/2019", "02/06/2019", "01/06/2019", "29/05/2019",
+      "26/05/2019", "25/05/2019", "22/05/2019", "19/05/2019", "18/05/2019", "15/05/2019", "12/05/2019", "11/05/2019", "08/05/2019", "05/05/2019",
+       "04/05/2019", "01/05/2019", "28/04/2019", "24/04/2019", "22/04/2019", "17/04/2019", "14/04/2019", "13/04/2019", "10/04/2019", "07/04/2019",
+        "03/04/2019", "31/03/2019", "30/03/2019", "27/03/2019", "24/03/2019", "23/03/2019", "20/03/2019", "17/03/2019", "13/03/2019", "10/03/2019",
+         "09/03/2019", "06/03/2019", "03/03/2019", "02/03/2019", "27/02/2019", "24/02/2019", "21/02/2019", "20/02/2019", "17/02/2019", "13/02/2019", "10/02/2019", "07/02/2019", "02/02/2019", "30/01/2019", "27/01/2019", "26/01/2019", "23/01/2019", "20/01/2019", "16/01/2019", "12/01/2019", "09/01/2019", "06/01/2019", "01/01/2019", "29/12/2018", "26/12/2018", "23/12/2018", "19/12/2018", "16/12/2018", "12/12/2018", "09/12/2018", "05/12/2018", "02/12/2018", "28/11/2018", "25/11/2018", "21/11/2018", "18/11/2018", "14/11/2018", "11/11/2018", "10/11/2018", "07/11/2018", "06/11/2018", "04/11/2018", "31/10/2018", "28/10/2018", "27/10/2018", "24/10/2018", "21/10/2018", "20/10/2018", "18/10/2018", "14/10/2018", "13/10/2018", "10/10/2018", "07/10/2018", "03/10/2018", "01/10/2018", "30/09/2018", "26/09/2018", "22/09/2018", "16/09/2018", "12/09/2018", "09/09/2018", "05/09/2018", "02/09/2018"]
+# "01/01/2020", 
 driver = webdriver.Firefox()
-driver.get(BASE_URL)
-driver.implicitly_wait(50)
+# driver.get(BASE_URL)
+# driver.implicitly_wait(50)
 
 def check_exists_by_xpath(xpath):
   try:
@@ -34,46 +48,61 @@ race_type_xpath = "/html/body/div/div[4]/table/tbody/tr[2]/td[1]"
 race_going_xpath = "/html/body/div/div[4]/table/tbody/tr[2]/td[3]"
 race_table_xpath = string.Template('''/html/body/div/div[5]/table/tbody/tr[$row]/td[$col]''')
 
-all_meets = (driver.find_elements_by_tag_name('option'))
+same_day_race_link_xpaths = "//div[2]/table/tbody/tr/td/a"
+table_row_xpath = "//div[5]/table/tbody/tr"
 
-same_day_race_links = [
-  "/html/body/div/div[2]/table/tbody/tr/td[3]/a",
-  "/html/body/div/div[2]/table/tbody/tr/td[4]/a",
-  "/html/body/div/div[2]/table/tbody/tr/td[5]/a",
-  "/html/body/div/div[2]/table/tbody/tr/td[6]/a",
-  "/html/body/div/div[2]/table/tbody/tr/td[7]/a",
-  "/html/body/div/div[2]/table/tbody/tr/td[8]/a",
-  "/html/body/div/div[2]/table/tbody/tr/td[9]/a",
-  "/html/body/div/div[2]/table/tbody/tr/td[10]/a",
-  "/html/body/div/div[2]/table/tbody/tr/td[11]/a"
-]
 
-all_race_entries = []
+count = 0
 
 # Begin grabbing data
-for meet in all_meets:
-  meet.click()
-  driver.implicitly_wait(10)
-  for same_day_link in same_day_race_links:
-    driver.implicitly_wait(10)
-    race_entries = []
-    # Get one race - 12 columns 8 rows + race name, going, track type
-    for row in range(1, 9):
-      race_table_vals = list()
-      race_table_vals.append(driver.find_element_by_xpath(race_name_xpath).text)
-      race_table_vals.append(driver.find_element_by_xpath(race_going_xpath).text)
-      race_table_vals.append(driver.find_element_by_xpath(race_type_xpath).text)
-      for col in range(1, 13):
-        rc = {'row': row, 'col': col}
-        if (check_exists_by_xpath(race_table_xpath.substitute(rc))):
-          race_table_vals.append(driver.find_element_by_xpath(race_table_xpath.substitute(rc)).text)
-      race_entries.append(race_table_vals)
-      race_table_vals = []
+for meet in dates:
+  all_race_entries = []
+  count += 1
+  if count % 5 == 0:
+    print("Progress: " + str(count) + " done")
+  driver.get(BASE_URL + meet)
+  same_day_selel = driver.find_elements_by_xpath(same_day_race_link_xpaths)[:-1]
+  same_day_links = [x.get_attribute("href") for x in same_day_selel]  
+  
+  # Get first race - x columns y rows + race name, going, track type
+  table_rows = driver.find_elements_by_xpath(table_row_xpath)
+  first_race_entry = []
+  if (check_exists_by_xpath(race_name_xpath)):
+    first_race_entry.append(driver.find_element_by_xpath(race_name_xpath).text)
+  if (check_exists_by_xpath(race_going_xpath)):
+    first_race_entry.append(driver.find_element_by_xpath(race_going_xpath).text)
+  if (check_exists_by_xpath(race_type_xpath)):
+    first_race_entry.append(driver.find_element_by_xpath(race_type_xpath).text)
 
-    print(race_entries)
-    all_race_entries.append(race_entries)
-    # Move on
-    driver.find_element_by_xpath(same_day_link).click()
+  for row in table_rows:
+    cols = row.find_elements_by_tag_name('td')
+    for col in cols:
+      first_race_entry.append(col.text)
+  all_race_entries.append(first_race_entry)
+  print(all_race_entries)
+  
+  # Get other races on same meet
+  for same_day_link in same_day_links:
+    driver.get(same_day_link)
 
+    # Scrape 2nd - n
+    race_entry = []
+    if (check_exists_by_xpath(race_name_xpath)):
+      race_entry.append(driver.find_element_by_xpath(race_name_xpath).text)
+    if (check_exists_by_xpath(race_going_xpath)):
+      race_entry.append(driver.find_element_by_xpath(race_going_xpath).text)
+    if (check_exists_by_xpath(race_type_xpath)):
+      race_entry.append(driver.find_element_by_xpath(race_type_xpath).text)
+
+    table_rows = driver.find_elements_by_xpath(table_row_xpath)
+    for row in table_rows:
+      cols = row.find_elements_by_tag_name('td')
+      for col in cols:
+        race_entry.append(col.text)
+    all_race_entries.append(race_entry)
+
+  # Save file as csv
+  df = pd.DataFrame(all_race_entries)
+  csv_data = df.to_csv("./races" + str(count) + ".csv",index=False)
 
 driver.quit()
