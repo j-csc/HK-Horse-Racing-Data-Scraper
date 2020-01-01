208d8881ff758930b441e2bb3444c78937ec3b1b
Jason Chen <superj63@hotmail.com>
2020-01-01 11:40:26 +0800

    finish scraper

--- scraper.py
+++ scraper.py
@@ -34,7 +34,7 @@ race_type_xpath = "/html/body/div/div[4]/table/tbody/tr[2]/td[1]"
 race_going_xpath = "/html/body/div/div[4]/table/tbody/tr[2]/td[3]"
 race_table_xpath = string.Template('''/html/body/div/div[5]/table/tbody/tr[$row]/td[$col]''')
 
-total_meets = (len(driver.find_elements_by_tag_name('option')))
+all_meets = (driver.find_elements_by_tag_name('option'))
 
 same_day_race_links = [
   "/html/body/div/div[2]/table/tbody/tr/td[3]/a",
@@ -47,29 +47,33 @@ same_day_race_links = [
   "/html/body/div/div[2]/table/tbody/tr/td[10]/a",
   "/html/body/div/div[2]/table/tbody/tr/td[11]/a"
 ]
+
 all_race_entries = []
 
-
 # Begin grabbing data
-for same_day_link in same_day_race_links:
+for meet in all_meets:
+  meet.click()
   driver.implicitly_wait(10)
-  race_entries = []
-  # Get one race - 12 columns 8 rows + race name, going, track type
-  for row in range(1, 9):
-    race_table_vals = list()
-    race_table_vals.append(driver.find_element_by_xpath(race_name_xpath).text)
-    race_table_vals.append(driver.find_element_by_xpath(race_going_xpath).text)
-    race_table_vals.append(driver.find_element_by_xpath(race_type_xpath).text)
-    for col in range(1, 13):
-      rc = {'row': row, 'col': col}
-      if (check_exists_by_xpath(race_table_xpath.substitute(rc))):
-        race_table_vals.append(driver.find_element_by_xpath(race_table_xpath.substitute(rc)).text)
-    race_entries.append(race_table_vals)
-    race_table_vals = []
+  for same_day_link in same_day_race_links:
+    driver.implicitly_wait(10)
+    race_entries = []
+    # Get one race - 12 columns 8 rows + race name, going, track type
+    for row in range(1, 9):
+      race_table_vals = list()
+      race_table_vals.append(driver.find_element_by_xpath(race_name_xpath).text)
+      race_table_vals.append(driver.find_element_by_xpath(race_going_xpath).text)
+      race_table_vals.append(driver.find_element_by_xpath(race_type_xpath).text)
+      for col in range(1, 13):
+        rc = {'row': row, 'col': col}
+        if (check_exists_by_xpath(race_table_xpath.substitute(rc))):
+          race_table_vals.append(driver.find_element_by_xpath(race_table_xpath.substitute(rc)).text)
+      race_entries.append(race_table_vals)
+      race_table_vals = []
 
-  print(race_entries)
-  all_race_entries.append(race_entries)
-  # Move on
-  driver.find_element_by_xpath(same_day_link).click()
+    print(race_entries)
+    all_race_entries.append(race_entries)
+    # Move on
+    driver.find_element_by_xpath(same_day_link).click()
+
 
 driver.quit()
