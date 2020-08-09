ff20a7bef605acba63e5d8f489a2e141a330e92f
Jason Chen <superj63@hotmail.com>
2020-08-08 18:24:43 -0700

    private stuff

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from Scrapers.scraper_race_dates import get_dates
-from Scrapers.scraper_racecard import scrape_racecard
-from Scrapers.scraper_racecard_info import scrape_racecard_info
-from Scrapers.scraper_horse_info_all import scrape_horse_info
-from Scrapers.scraper_horse_veterinary_records import scrape_horse_veterinary_records
-from Scrapers.scraper_penetrometer import scrape_penetrometer
-from Scrapers.scraper_horse_roarers import scrape_horse_roarer
+from scrapers.scraper_race_dates import get_dates
+from scrapers.scraper_racecard import scrape_racecard
+from scrapers.scraper_racecard_info import scrape_racecard_info
+from scrapers.scraper_horse_info_all import scrape_horse_info
+from scrapers.scraper_horse_veterinary_records import scrape_horse_veterinary_records
+from scrapers.scraper_penetrometer import scrape_penetrometer
+from scrapers.scraper_horse_roarers import scrape_horse_roarer
 import config
 
 def main():
